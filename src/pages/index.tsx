9afd28d7a2275c96aa85f76adaa02d5ab369f091
unknown <lucas11souza97@gmail.com>
2023-07-24 22:21:14 -0400

    feat; add lint

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Heading } from "@ignite-ui/react"
+import { globalStyles } from "../styles/global"
+
+
+globalStyles()
+export default function Home() {
+  return (
+    <Heading>hello</Heading>
+  )
+}
---
+++ src/pages/teste.tsx
@@ -0,0 +1,22 @@
+// import { globalStyles } from "../styles/global"
+
+
+// globalStyles()
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode
+// }) {
+//   return (
+//     <html lang="en">
+//       <body>{children}</body>
+//     </html>
+//   )
+// }
+
+
+// import { AppProps } from "next/app"
+
+// export default function App({Component, pageProps}: AppProps) {
+//   return <Component {...pageProps} />
+// }
