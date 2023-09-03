ec83be8f85e1a30dec18311699186097e6c56bd6
unknown <lucas11souza97@gmail.com>
2023-09-03 13:13:17 -0400

    feat: add cookies in Next

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,5 +1,38 @@
+import { setCookie } from 'nookies'
+import { prisma } from '../../../../src/lib/prisma'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).json(req.body)
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).end()
+  }
+
+  const { name, username } = req.body
+
+  const userExists = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (userExists) {
+    return res.status(400).json({
+      message: 'Username already taken.',
+    })
+  }
+  const user = await prisma.user.create({
+    data: {
+      name,
+      username,
+    },
+  })
+
+  setCookie({ res }, '@call:userId', user.id, {
+    maxAge: 60 * 60 * 24 * 7, // 7 days
+    path: '/',
+  })
+  return res.status(201).json(user)
 }
